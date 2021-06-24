f4240342e27e7382cafdaa50d078765d42b72637
seunghyo <tmdgy6232@naver.com>
2021-06-24 19:30:03 +0900

    html for문 사용, post redirect 사용

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
 from accountapp.models import HelloWorld
 
 
@@ -12,6 +14,7 @@ def hello_world(request):
         new_hello_world = HelloWorld()
         new_hello_world.text = temp
         new_hello_world.save()
-        return render(request, 'accountapp/hello_world.html',context={'hello_world_output' : new_hello_world})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else :
-        return render(request, 'accountapp/hello_world.html', context={'text': 'get method!!!'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
