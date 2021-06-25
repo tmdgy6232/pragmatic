9d4360ddae65a2abf8967f03536944b506f3c692
seunghyo <tmdgy6232@naver.com>
2021-06-25 14:54:23 +0900

    회원 탈퇴기능 구현

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = 'accountapp'
 
@@ -26,5 +26,6 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name = 'logout'),
     path('create/', AccountCreateView.as_view(), name = 'create'),
     path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
-    path('update/<int:pk>', AccountUpdateView.as_view(), name='update')
+    path('update/<int:pk>', AccountUpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', AccountDeleteView.as_view(), name='delete'),
 ]
