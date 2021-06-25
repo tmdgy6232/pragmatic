3e4ec53306d8d19cbc3bf54bf397d895cc50dc3d
seunghyo <tmdgy6232@naver.com>
2021-06-25 12:32:18 +0900

    account detailView function apply

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
-from accountapp.views import hello_world, AccountCreateView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView
 
 app_name = 'accountapp'
 
@@ -24,5 +24,6 @@ urlpatterns = [
     path('hello_world/', hello_world, name = 'hello_world'),
     path('login/', LoginView.as_view(template_name = 'accountapp/login.html'), name = 'login'),
     path('logout/', LogoutView.as_view(), name = 'logout'),
-    path('create/', AccountCreateView.as_view(), name = 'create')
+    path('create/', AccountCreateView.as_view(), name = 'create'),
+    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail')
 ]
