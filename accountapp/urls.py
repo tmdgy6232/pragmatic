41d1db57d1cef9e8dc5d0ebdba3b9a622d997fa1
seunghyo <tmdgy6232@naver.com>
2021-06-25 13:14:20 +0900

    회원정보 변경 기능 구현

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
-from accountapp.views import hello_world, AccountCreateView, AccountDetailView
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView
 
 app_name = 'accountapp'
 
@@ -25,5 +25,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name = 'accountapp/login.html'), name = 'login'),
     path('logout/', LogoutView.as_view(), name = 'logout'),
     path('create/', AccountCreateView.as_view(), name = 'create'),
-    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail')
+    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
+    path('update/<int:pk>', AccountUpdateView.as_view(), name='update')
 ]
