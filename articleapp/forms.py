418d95fe90172c080914823e1dd47a7c9dfcb814
tmdgy6232 <tmdgy6232@naver.com>
2021-07-10 22:40:37 +0900

    WYSIWYG medium Editor 추가

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -1,9 +1,16 @@
+from django import forms
 from django.forms import ModelForm
 
 from articleapp.models import Article
+from projectapp.models import Project
 
 
 class ArticleCreationForm(ModelForm):
+    # medium editor를 적용하기위해 class명 커스터마이징
+    content = forms.CharField(widget=forms.Textarea(attrs={'class':'editable',
+                                                           'style':'height:auto; text-align:left;'}))
+
+    project = forms.ModelChoiceField(queryset=Project.objects.all(), required=False)
     class Meta:
         model = Article
         fields = ['title', 'image', 'project','content']
